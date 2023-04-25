be164cf18f08aeb230fd7e845225ab122d17b590
Fie Langmann <fielangmann@gmail.com>
2023-04-25 15:40:25 +0200

    Basics and loading packages tested. First commit

---
+++ R/learning.R
@@ -0,0 +1,15 @@
+# Basics of R -------------------------------------------------------------
+
+colnames(airquality)
+str(airquality)
+summary(airquality)
+
+# Automatic styling
+1 + 1 # highlight and ctrl+shift+a gives:
+1 + 1
+
+mean(2:6) # ctrl+shift+p --> style --> style_active_file (will alter the whole file, you're working in )
+
+# Loading packages --------------------------------------------------------
+
+library(tidyverse)
